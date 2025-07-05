87b8086ef3df20d0748f1ceacd1612510fa27b77
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 16:52:28 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -2,5 +2,9 @@ variable "db_name" {}
 variable "db_username" {}
 variable "db_password" {}
 variable "subnet_ids" { type = list(string) }
-variable "security_groups" { type = list(string) }
 variable "tags" { type = map(string) }
+variable "security_groups" {
+  type    = list(string)
+  default = []
+}
+
