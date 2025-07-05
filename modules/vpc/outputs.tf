21e224410dd8873795274b28b4740f92da506f7c
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 11:53:23 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnets" {
-  value = module.vpc.public_subnets
+  value = module.vpc_core.vpc_id
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value = module.vpc_core.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc_core.public_subnets
 }
 
 output "default_security_group_id" {
-  value = module.vpc.default_security_group_id
+  value = module.vpc_core.default_security_group_id
 }
