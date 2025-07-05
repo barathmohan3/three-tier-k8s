31b81d2d054d5328013e5f957e525282e731a063
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 14:23:32 +0530

    Update main.tf

--- modules/secrets_manager/main.tf
+++ modules/secrets_manager/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "db-credentials"
+  name = "db-credentials-v2"
 }
 
 resource "aws_secretsmanager_secret_version" "db_credentials_version" {
