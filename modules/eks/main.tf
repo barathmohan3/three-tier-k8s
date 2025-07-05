0412afe59f16d634e5ef54d2c1568c8d96ec295d
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 11:31:58 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,8 +3,8 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  subnets         = var.subnets
   vpc_id          = var.vpc_id
+  vpc_subnet_ids  = var.vpc_subnet_ids
 
   eks_managed_node_groups = {
     backend = {
