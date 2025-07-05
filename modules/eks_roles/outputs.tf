014dd5a763caa56d0d32c661232ca692d95683dd
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 23:10:27 +0530

    Update outputs.tf

--- modules/eks_roles/outputs.tf
+++ modules/eks_roles/outputs.tf
@@ -5,3 +5,7 @@ output "cluster_role_arn" {
 output "node_role_arn" {
   value = aws_iam_role.eks_node_role.arn
 }
+
+output "dependency" {
+  value = [aws_iam_role.cluster_role, aws_iam_role.node_role]
+}
