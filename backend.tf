e4eb1c955be2942aa39d483ee21b74e691b128e7
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 13:15:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-oidc-bucket-15328069840"
-    key            = "whey/terraform.tfstate"
+    key            = "wheypro/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = false
