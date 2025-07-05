6b3a455a3c8330a1d65df6fb80096a6b18bcaf51
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 12:24:14 +0530

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,11 +1,11 @@
 output "alb_dns_name" {
-  value = module.alb_core.this_lb_dns_name
+  value = module.alb_core.lb_dns_name
 }
 
 output "alb_arn" {
-  value = module.alb_core.this_lb_arn
+  value = module.alb_core.lb_arn
 }
 
 output "alb_security_group_id" {
-  value = module.alb_core.this_security_group_id
+  value = module.alb_core.security_group_id
 }
