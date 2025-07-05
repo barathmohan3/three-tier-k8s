2967e45f86e504c7499ed1167554db90b40c8973
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 11:32:24 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -3,3 +3,6 @@ variable "cluster_version" {}
 variable "vpc_id" {}
 variable "subnets" { type = list(string) }
 variable "tags" { type = map(string) }
+variable "vpc_subnet_ids" {
+  type = list(string)
+}
