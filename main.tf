2d8f4f84ac2aa9da0d0b670aed9625cceb39d9b0
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 16:43:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,6 @@ module "rds" {
   db_username      = var.db_username
   db_password      = var.db_password
   subnet_ids       = module.vpc.private_subnets
-  security_groups  = [aws_security_group.rds_sg.id]
   tags             = var.tags
 }
 
