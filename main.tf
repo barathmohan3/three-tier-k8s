dfc2a41c998ba17f45a2c38c6f7a8da52bd02643
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 21:29:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,17 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "vpc" {
-  source          = "./modules/vpc"
-  vpc_cidr        = var.vpc_cidr
-  azs             = var.azs
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  tags            = var.tags
+module "eks" {
+  source = "./modules/eks"
+
+  name                        = var.cluster_name
+  public_subnets              = module.vpc.public_subnets
+  private_subnets             = module.vpc.private_subnets
+  cluster_role_arn            = module.eks_roles.cluster_role_arn
+  node_role_arn               = module.eks_roles.node_role_arn
+  security_group_ids          = [module.vpc.default_security_group_id]
+  cluster_role_dependency     = module.eks_roles.dependency
+  eks_oidc_root_ca_thumbprint = data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint
 }
 
 module "eks_roles" {
