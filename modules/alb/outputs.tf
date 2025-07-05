d453d82c90324963bd723541b11f83eb3b1bd8ab
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 12:22:02 +0530

    Update outputs.tf

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,14 +1,11 @@
 output "alb_dns_name" {
-  description = "DNS name of the ALB"
-  value       = module.alb_core.this_lb_dns_name
+  value = module.alb_core.this_lb_dns_name
 }
 
 output "alb_arn" {
-  description = "ARN of the ALB"
-  value       = module.alb_core.this_lb_arn
+  value = module.alb_core.this_lb_arn
 }
 
 output "alb_security_group_id" {
-  description = "Security group ID of the ALB"
-  value       = module.alb_core.this_security_group_id
+  value = module.alb_core.this_security_group_id
 }
