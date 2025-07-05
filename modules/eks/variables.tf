c0ae57ea55a31fe84d411dfa3a8fb9936848bfdd
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 21:52:14 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,24 +1,39 @@
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
+variable "name" {
+  description = "Name prefix for resources"
   type        = string
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "Subnet IDs"
+variable "public_subnets" {
+  description = "Public subnet IDs"
   type        = list(string)
 }
 
-variable "tags" {
-  description = "Tags to apply"
-  type        = map(string)
+variable "private_subnets" {
+  description = "Private subnet IDs"
+  type        = list(string)
+}
+
+variable "cluster_role_arn" {
+  description = "EKS Cluster Role ARN"
+  type        = string
+}
+
+variable "node_role_arn" {
+  description = "EKS Node Role ARN"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "Security group IDs for the EKS control plane"
+  type        = list(string)
+}
+
+variable "cluster_role_dependency" {
+  description = "Dependency to ensure IAM role is created before the cluster"
+  type        = any
+}
+
+variable "eks_oidc_root_ca_thumbprint" {
+  description = "OIDC root CA thumbprint for IRSA"
+  type        = string
 }
