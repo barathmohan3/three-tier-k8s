3e89ba18840003abdeec2a161735bd3edad070f2
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 12:23:59 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,34 +1,46 @@
 module "alb_core" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "6.6.0"
+  source  = "terraform-aws-modules/load-balancer/aws"
+  version = "~> 2.0"
 
   name               = "support-portal-alb"
   load_balancer_type = "application"
-  vpc_id             = var.vpc_id
-  subnets            = var.public_subnets
-  security_groups    = var.security_groups
 
-  create_lb = true
-  enable_deletion_protection = false
+  vpc_id  = var.vpc_id
+  subnets = var.public_subnets
 
-  target_groups = {
-    frontend = {
+  security_groups = var.security_groups
+
+  target_groups = [
+    {
+      name_prefix      = "frontend"
       backend_protocol = "HTTP"
       backend_port     = 80
-    }
-    backend = {
+      target_type      = "ip"
+      deregistration_delay = 5
+    },
+    {
+      name_prefix      = "backend"
       backend_protocol = "HTTP"
       backend_port     = 5000
+      target_type      = "ip"
+      deregistration_delay = 5
     }
-  }
+  ]
 
-  listeners = {
-    http = {
+  listeners = [
+    {
       port     = 80
       protocol = "HTTP"
-      forward  = ["frontend"]
+      default_action = {
+        type             = "fixed-response"
+        fixed_response   = {
+          content_type = "text/plain"
+          message_body = "OK"
+          status_code  = "200"
+        }
+      }
     }
-  }
+  ]
 
   tags = var.tags
 }
