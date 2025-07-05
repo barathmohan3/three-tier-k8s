91867f1dcb2a0aeb04a96803c79a1f9e291b2715
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 12:22:19 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,4 +1,10 @@
 variable "vpc_id" {}
-variable "public_subnets" { type = list(string) }
-variable "security_groups" { type = list(string) }
-variable "tags" { type = map(string) }
+variable "public_subnets" {
+  type = list(string)
+}
+variable "security_groups" {
+  type = list(string)
+}
+variable "tags" {
+  type = map(string)
+}
