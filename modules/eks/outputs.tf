b5822601f98b38d8c964f9e2014dfc13cab59961
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 21:12:34 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,11 +1,3 @@
-output "cluster_name" {
-  value = module.eks_core.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks_core.cluster_endpoint
-}
-
-output "cluster_ca_data" {
-  value = module.eks_core.cluster_certificate_authority_data
+output "eks_endpoint" {
+  value = module.eks.cluster_endpoint
 }
