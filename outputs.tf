4fd0e23e0af804a92be268e17ea44a527b4f2592
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 12:35:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,17 +8,12 @@ output "alb_arn" {
   value       = module.alb.lb_arn
 }
 
-output "alb_security_group_id" {
-  description = "Security group ID of the ALB"
-  value       = module.alb.security_group_id
-}
-
 output "http_listener_arns" {
   description = "HTTP Listener ARNs"
   value       = module.alb.http_listener_arns
 }
 
 output "target_group_arns" {
-  description = "Target group ARNs"
+  description = "Target Group ARNs"
   value       = module.alb.target_group_arns
 }
