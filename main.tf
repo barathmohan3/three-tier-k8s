b60c33e893d93d3cce3f88f2a6ec882e1e6d7dab
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-05 12:55:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "secrets_manager" {
 
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "9.6.0"
+  version = "6.6.0"
 
   name               = "support-portal-alb"
   load_balancer_type = "application"
@@ -50,38 +50,32 @@ module "alb" {
   subnets         = module.vpc.public_subnets
   security_groups = [module.vpc.default_security_group_id]
 
-  target_groups = {
-    frontend = {
-      name_prefix      = "fe"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "frontend"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "ip"
-    }
-    backend = {
-      name_prefix      = "be"
+    },
+    {
+      name_prefix      = "backend"
       backend_protocol = "HTTP"
       backend_port     = 5000
       target_type      = "ip"
     }
-  }
-
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
-      default_action = {
-        type = "fixed-response"
-        fixed_response = {
-          content_type = "text/plain"
-          message_body = "ok"
-          status_code  = "200"
-        }
-      }
-    }
-  }
+  ]
 
   tags = var.tags
 }
+
 
 module "ecr" {
   source = "./modules/ecr"
